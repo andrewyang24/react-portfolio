03a27e70361359f8d1af821810ca33ece3667764
Andrew <ayang24@email.unc.edu>
2022-10-05 13:47:06 -0400

    changed json

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
+import Loader from 'react-loaders'
 import './index.scss'
 
 
@@ -18,6 +19,7 @@ const Home = () => {
     }, [])
 
     return (
+        <>
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
@@ -42,6 +44,8 @@ const Home = () => {
             </div>
             <Logo />
         </div>
+        <Loader type="pacman" />
+        </>
     )
 }
 

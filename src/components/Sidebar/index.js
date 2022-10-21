2cb365c5f7fb6cfe98c94bab9915114738ebdaa3
Andrew <ayang24@email.unc.edu>
2022-10-21 12:43:15 -0400

    made responsive nav

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -10,10 +10,14 @@ import {
   faHome,
   faUser,
   faEnvelope,
+  faBars,
+  faClose,
 } from '@fortawesome/free-solid-svg-icons'
 import { Link, NavLink } from 'react-router-dom'
+import { useState } from 'react'
 
 const Sidebar = () => {
+  const[showNav, setShowNav] = useState(false);
 
   return (
     <div className="nav-bar">
@@ -21,7 +25,7 @@ const Sidebar = () => {
         <img src={LogoS} alt="Logo" />
         <img className="sub-logo" src={LogoSubtitle} alt="Andrew Yang" />
       </Link>
-      <nav>
+      <nav class={showNav ? 'mobile-show' : ''}>
         <NavLink 
           exact="true" 
           activeclassname="active" 
@@ -45,6 +49,13 @@ const Sidebar = () => {
         >
           <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className="close-icon"
+        />
       </nav>
       <ul>
         <li>
@@ -74,6 +85,13 @@ const Sidebar = () => {
           </a>
         </li>
       </ul>
+      <FontAwesomeIcon 
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className='hamburger-icon'
+      />
     </div>
   )
 }
